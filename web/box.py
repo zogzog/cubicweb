4af664026ce322096fdeec14eb833704d782dcd6
Sylvain Thenault <sylvain.thenault@logilab.fr>
2008-12-05 18:05:34 +0100

    add _rql_condition selector to default EntityBox selectors

--- web/box.py
+++ web/box.py
@@ -14,8 +14,9 @@ from cubicweb.common.registerers import (accepts_registerer,
                                       extresources_registerer,
                                       etype_rtype_priority_registerer)
 from cubicweb.common.selectors import (etype_rtype_selector, onelinerset_selector,
-                                    accept_selector, accept_rtype_selector,
-                                    primaryview_selector, contextprop_selector)
+                                       accept_selector, accept_rtype_selector,
+                                       primaryview_selector, contextprop_selector,
+                                       _rqlcondition_selector)
 from cubicweb.common.view import Template
 from cubicweb.common.appobject import ReloadableMixIn
 
@@ -151,7 +152,8 @@ class EntityBoxTemplate(BoxTemplate):
     __registerer__ = accepts_registerer
     __selectors__ = (onelinerset_selector, primaryview_selector,
                      contextprop_selector, etype_rtype_selector,
-                     accept_rtype_selector, accept_selector)
+                     accept_rtype_selector, accept_selector,
+                     _rqlcondition_selector)
     accepts = ('Any',)
     context = 'incontext'
     
