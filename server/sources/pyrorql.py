e1b2708cdddf1936ac26e3a9202be41a4c618b52
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-01-14 19:39:32 +0100

    oops, get back cross_relation from mapping to rql source

--- server/sources/pyrorql.py
+++ server/sources/pyrorql.py
@@ -117,6 +117,7 @@ repository (default to 5 minutes).',
         self.support_entities = mapping['support_entities']
         self.support_relations = mapping.get('support_relations', {})
         self.dont_cross_relations = mapping.get('dont_cross_relations', ())
+        self.cross_relations = mapping.get('cross_relations', ())
         baseurl = source_config.get('base-url')
         if baseurl and not baseurl.endswith('/'):
             source_config['base-url'] += '/'
