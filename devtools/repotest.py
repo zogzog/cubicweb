d6f973faa5619205349b65fd42cdfc9276882bb0
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-02-11 17:55:44 +0100

    hop

--- devtools/repotest.py
+++ devtools/repotest.py
@@ -3,7 +3,7 @@
 This module contains functions to initialize a new repository.
 
 :organization: Logilab
-:copyright: 2003-2008 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+:copyright: 2003-2009 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 :contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
 """
 __docformat__ = "restructuredtext en"
