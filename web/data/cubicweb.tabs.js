4883bdbc7bca5f70e5c3ccc54b7693eb2808e39b
Aurelien Campeas <aurelien.campeas@logilab.fr>
2009-02-11 19:11:55 +0100

    [tabs] reuse the new set_cookie controller method

--- web/data/cubicweb.tabs.js
+++ web/data/cubicweb.tabs.js
@@ -1,7 +1,6 @@
-function set_tab(tabname) {
+function set_tab(tabname, cookiename) {
     // set appropriate cookie
-    // XXX see if we can no just do it with jQuery
-    async_remote_exec('remember_active_tab', tabname);
+    async_remote_exec('set_cookie', cookiename, tabname);
     // trigger show + tabname event
     trigger_load(tabname);
 }
