71a35ee279d46ef0dc5d73885d8a55bd229696ff
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2009-02-19 22:17:39 +0100

    update test, use a DateTimePicker widget
    --HG--
    branch : tls-sprint

--- web/test/unittest_form.py
+++ web/test/unittest_form.py
@@ -5,24 +5,18 @@ from cubicweb.web.views.baseforms import ChangeStateForm
 
 class CustomChangeStateForm(ChangeStateForm):
     hello = IntField(name='youlou')
-
+    creation_date = DateTimeField(widget=DateTimePicker)
+    
 class EntityFieldsFormTC(EnvBasedTC):
 
     def setUp(self):
         super(EntityFieldsFormTC, self).setUp()
-##         def next_tabindex(self):
-##             self.count += 1
-##             return self.count
-##         self.req = mock_object(build_url=lambda *args,**kwargs: 'myurl.com/pouet',
-##                                url=lambda *args,**kwargs: 'myurl.com/form',
-##                                _=lambda s,x: x, form={}, data={},
-##                                next_tabindex=next_tabindex, count=0)
-##         self.entity = mock_object(eid=1, has_eid=lambda x: False, id='Entity')
+        self.req = self.request()
+        self.entity = self.user(self.req)
         
     def test(self):
-        req = self.request()
-        form = ChangeStateForm(req, redirect_path='perdu.com')
-        self.assertEquals(form.form_render(self.user(req), state=123),
+        form = ChangeStateForm(self.req, redirect_path='perdu.com')
+        self.assertEquals(form.form_render(self.entity, state=123),
                           ''' ''')
 
     def test_form_inheritance(self):
