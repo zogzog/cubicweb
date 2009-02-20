44343aac70e33eefc08058f4a09992a95d03a8ad
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2009-02-20 17:38:38 +0100

    select needs to accept an id
    --HG--
    branch : tls-sprint

--- common/tags.py
+++ common/tags.py
@@ -15,11 +15,14 @@ img = tag('img')
 label = tag('label')
 option = tag('option')
 
-def select(name, multiple=False, options=[]):
+def select(name, id=None, multiple=False, options=[]):
+    attrs = {}
     if multiple:
-        html = [u'<select name="%s" multiple="multiple">' % name]
-    else:
-        html = [u'<select name="%s">' % name]
+        attrs['multiple'] = 'multiple'
+    if id:
+        attrs['id'] = id
+    html = [u'<select name="%s" %s>' % (name,
+                                        ' '.join('%s="%s"' % kv for kv in attrs.items()))]
     html += options
     html.append(u'</select>')
     return u'\n'.join(html)
