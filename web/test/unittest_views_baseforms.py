f86e2d13f5eb91a0f63d52c564337f3575710058
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-02-25 11:02:14 +0100

    teardown_module now gets test results as arguement

--- web/test/unittest_views_baseforms.py
+++ web/test/unittest_views_baseforms.py
@@ -18,7 +18,7 @@ def setup_module(options):
         return DateTime(0000, 1, 1)
     widgets.today = widgets.now = _today
 
-def teardown_module(options):
+def teardown_module(options, results):
     widgets.today = orig_today
     widgets.now = orig_now
 
