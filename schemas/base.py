ddbaff6131f656b3fa88315dadb9a00434f09c06
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-04-08 20:30:58 +0200

    copyright update
    --HG--
    branch : tls-sprint

--- schemas/base.py
+++ schemas/base.py
@@ -1,7 +1,7 @@
 """core CubicWeb schema, but not necessary at bootstrap time
 
 :organization: Logilab
-:copyright: 2001-2008 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+:copyright: 2001-2009 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 :contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
 """
 __docformat__ = "restructuredtext en"
