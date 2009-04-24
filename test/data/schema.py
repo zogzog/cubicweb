67e4bd9c0d1557a4baffe16145d81e9f17b6c948
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-04-24 16:48:38 +0200

    delete-trailing-whitespaces
    --HG--
    branch : tls-sprint

--- test/data/schema.py
+++ test/data/schema.py
@@ -5,11 +5,11 @@ class Personne(EntityType):
     travaille = SubjectRelation('Societe')
     evaluee = SubjectRelation(('Note', 'Personne'))
     connait = SubjectRelation('Personne', symetric=True)
-    
+
 class Societe(EntityType):
     nom = String()
     evaluee = SubjectRelation('Note')
-    
+
 class Note(EntityType):
     type = String()
     ecrit_par = SubjectRelation('Personne')
