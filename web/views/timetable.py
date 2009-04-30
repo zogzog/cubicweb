78a2774d1f8a0c813a470797fb4daeec71c9d63b
sylvain.thenault@logilab.fr <sylvain.thenault@logilab.fr>
2009-04-30 10:46:50 +0200

    datetime fixes use weekday() instead of day_of_week
    --HG--
    branch : tls-sprint

--- web/views/timetable.py
+++ web/views/timetable.py
@@ -164,7 +164,7 @@ class TimeTableView(AnyRsetView):
             previous_is_empty = False
 
             klass = "even"
-            if date.day_of_week in (5, 6) and not empty_line:
+            if date.weekday() in (5, 6) and not empty_line:
                 klass = "odd"
             self.w(u'<tr class="%s">' % klass)
             odd = not odd
