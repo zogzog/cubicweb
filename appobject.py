4204679d00f015321f23c6642a987723665e9d41
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2009-06-10 23:29:37 +0200

    [CWCache] yet another bugfix in cwcache: proper initalization of Cache.creation_date
    --HG--
    branch : stable

--- appobject.py
+++ appobject.py
@@ -25,8 +25,9 @@ ONESECOND = timedelta(0, 1, 0)
 class Cache(dict):
     def __init__(self):
         super(Cache, self).__init__()
-        self.cache_creation_date = None
-        self.latest_cache_lookup = datetime.now()
+        _now = datetime.now()
+        self.cache_creation_date = _now
+        self.latest_cache_lookup = _now
 
 CACHE_REGISTRY = {}
 
