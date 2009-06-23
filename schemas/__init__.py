504d2124004d693e98c297a1d7318d2e228001dd
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-06-23 13:31:35 +0200

    schema should now be importable

---
+++ schemas/__init__.py
@@ -0,0 +1,13 @@
+
+META_ETYPE_PERMS = {
+    'read':   ('managers', 'users', 'guests',),
+    'add':    ('managers',),
+    'delete': ('managers',),
+    'update': ('managers', 'owners',),
+    }
+
+META_RTYPE_PERMS = {
+    'read':   ('managers', 'users', 'guests',),
+    'add':    ('managers',),
+    'delete': ('managers',),
+    }
