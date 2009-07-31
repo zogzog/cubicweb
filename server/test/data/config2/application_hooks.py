4d263a92ebb1b156348e0119d397b8a642ff0163
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-07-31 19:43:18 +0200

    [D server test] remove deprecated test on application_hooks file, and associated data

--- server/test/data/config2/application_hooks.py
+++
@@ -1,8 +0,0 @@
-"""hooks for config2
-
- Copyright (c) 2003-2007 LOGILAB S.A. (Paris, FRANCE).
- http://www.logilab.fr/ -- mailto:contact@logilab.fr
-:license: GNU Lesser General Public License, v2.1 - http://www.gnu.org/licenses
-"""
-
-HOOKS = {"after_delete_relation" : {"todo_by" : [lambda: 1]}}
