0f9faef3726f3f9269f8725ba648af1930e49e7e
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-08-01 16:54:03 +0200

    explicit imports

--- test/data/rrqlexpr_on_eetype.py
+++ test/data/rrqlexpr_on_eetype.py
@@ -5,6 +5,9 @@
 :contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
 :license: GNU Lesser General Public License, v2.1 - http://www.gnu.org/licenses
 """
+from yams.buildobjs import EntityType, String
+from cubicweb.schema import RRQLExpression
+
 class ToTo(EntityType):
     permissions = {
         'read': ('managers', RRQLExpression('S bla Y'),),
