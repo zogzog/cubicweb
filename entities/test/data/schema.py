323b6d647d6cb5d9d14ec44ed8d5f10f03d61c32
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-08-20 17:52:22 +0200

    cleanup
    --HG--
    branch : 3.5

--- entities/test/data/schema.py
+++ entities/test/data/schema.py
@@ -1,10 +1,11 @@
-"""
+"""entities tests schema
 
 :organization: Logilab
 :copyright: 2001-2009 LOGILAB S.A. (Paris, FRANCE), license is LGPL v2.
 :contact: http://www.logilab.fr/ -- mailto:contact@logilab.fr
 :license: GNU Lesser General Public License, v2.1 - http://www.gnu.org/licenses
 """
+
 from yams.buildobjs import EntityType, String
 from cubicweb.schema import make_workflowable
 
