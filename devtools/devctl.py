b219de4e4e21972d1ab9eb344ea5cc5444180050
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-09-15 14:10:52 +0200

    [i18ncube] access to appearsin_addmenu config through uicfg directly
    --HG--
    branch : 3.5

--- devtools/devctl.py
+++ devtools/devctl.py
@@ -23,6 +23,7 @@ from logilab.common.clcommands import register_commands
 from cubicweb import CW_SOFTWARE_ROOT as BASEDIR, BadCommandUsage, underline_title
 from cubicweb.__pkginfo__ import version as cubicwebversion
 from cubicweb.toolsutils import Command, copy_skeleton
+from cubicweb.web import uicfg
 from cubicweb.web.webconfig import WebConfiguration
 from cubicweb.server.serverconfig import ServerConfiguration
 
@@ -151,7 +152,7 @@ def _generate_schema_pot(w, vreg, schema, libconfig=None, cube=None):
             add_msg(w, rschema.description)
     w('# add related box generated message\n')
     w('\n')
-    actionbox = vreg['boxes']['edit_box'][0]
+    appearsin_addmenu = uicfg.actionbox_appearsin_addmenu
     for eschema in schema.entities():
         if eschema.is_final():
             continue
@@ -170,8 +171,8 @@ def _generate_schema_pot(w, vreg, schema, libconfig=None, cube=None):
                             subjtype, objtype = teschema, eschema
                         if librschema.has_rdef(subjtype, objtype):
                             continue
-                    if actionbox.appearsin_addmenu.etype_get(eschema, rschema,
-                                                             role, teschema):
+                    if appearsin_addmenu.etype_get(eschema, rschema, role,
+                                                   teschema):
                         if role == 'subject':
                             label = 'add %s %s %s %s' % (eschema, rschema,
                                                          teschema, role)
