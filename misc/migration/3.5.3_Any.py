8309dc0ae396dabfc188380484e6eaabfcc350c9
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2009-10-07 22:18:16 +0200

    [migration] make it more explicit that BaseTransition upgrade should only be done if migrating from version prior to 3.5
    --HG--
    branch : stable

--- misc/migration/3.5.3_Any.py
+++ misc/migration/3.5.3_Any.py
@@ -1,8 +1,7 @@
-sync_schema_props_perms('state_of')
-sync_schema_props_perms('transition_of')
-
 # type attribute might already be there if migrating from
 # version < 3.5 to version >= 3.5.3, BaseTransition being added
 # in bootstrap_migration
-if not schema.eschema('BaseTransition').has_subject_relation('type'):
+if versions_map['cubicweb'][0] < (3, 5, 0):
     add_attribute('BaseTransition', 'type')
+    sync_schema_props_perms('state_of')
+    sync_schema_props_perms('transition_of')
