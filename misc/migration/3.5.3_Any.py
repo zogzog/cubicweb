2ec748221b61ec2006736cc4df59f8a464124eb3
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2009-10-07 21:59:22 +0200

    [migration] fix 3.5.3_Any.py for migrations starting from before 3.5
    --HG--
    branch : stable

--- misc/migration/3.5.3_Any.py
+++ misc/migration/3.5.3_Any.py
@@ -1,4 +1,8 @@
 sync_schema_props_perms('state_of')
 sync_schema_props_perms('transition_of')
 
-add_attribute('BaseTransition', 'type')
+# type attribute might already be there if migrating from
+# version < 3.5 to version >= 3.5.3, BaseTransition being added
+# in bootstrap_migration
+if not schema.eschema('BaseTransition').has_subject_relation('type'):
+    add_attribute('BaseTransition', 'type')
