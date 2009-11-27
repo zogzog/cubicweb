264deb2d61ed49840f89d9694c9d93e2e08006a6
Sylvain Thénault <sylvain.thenault@logilab.fr>
2009-11-27 09:36:42 +0100

    don't type __maineid, it may not be an eid actually
    --HG--
    branch : stable

--- web/views/editcontroller.py
+++ web/views/editcontroller.py
@@ -45,8 +45,7 @@ class EditController(ViewController):
         form = req.form
         # so we're able to know the main entity from the repository side
         if '__maineid' in form:
-            req.set_shared_data('__maineid', typed_eid(form['__maineid']),
-                                querydata=True)
+            req.set_shared_data('__maineid', form['__maineid'], querydata=True)
         # no specific action, generic edition
         self._to_create = req.data['eidmap'] = {}
         self._pending_relations = []
