abc543497825f33c2ca96c0454a3356724bc92aa
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-02-08 09:57:25 +0100

    errors on postcommit should never occurs, log it as critical
    --HG--
    branch : stable

--- server/session.py
+++ server/session.py
@@ -474,7 +474,8 @@ class Session(RequestSessionMixIn):
                 try:
                     operation.handle_event('%s_event' % trstate)
                 except:
-                    self.exception('error while %sing', trstate)
+                    self.critical('error while %sing', trstate,
+                                  exc_info=sys.exc_info())
             self.debug('%s session %s done', trstate, self.id)
         finally:
             self._touch()
