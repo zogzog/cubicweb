bb0c991e7acb3ea470f3aba0c41d2aa50e75ee6a
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-03-04 12:08:09 +0100

    oops, 'not' missing

--- pytestconf.py
+++ pytestconf.py
@@ -21,7 +21,7 @@ class CustomPyTester(PyTester):
 
 def clean_repo_test_cls(cls):
     if 'repo' in cls.__dict__:
-        if cls.repo._shutting_down:
+        if not cls.repo._shutting_down:
             cls.repo.shutdown()
         del cls.repo
     for clsattr in ('cnx', '_orig_cnx', 'config', '_config', 'vreg', 'schema'):
