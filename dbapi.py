c45c4ea96cd81018a1523f6e26eef74449c1a1c0
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-03-12 15:00:54 +0100

    [dbapi] return default argument instead of None when no connection set yet

--- dbapi.py
+++ dbapi.py
@@ -237,7 +237,7 @@ class DBAPIRequest(RequestSessionBase):
     def get_session_data(self, key, default=None, pop=False):
         """return value associated to `key` in session data"""
         if self.cnx is None:
-            return None # before the connection has been established
+            return default # before the connection has been established
         return self.cnx.get_session_data(key, default, pop)
 
     def set_session_data(self, key, value):
