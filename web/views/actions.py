4069018f91d0b4cf6686bb0259b039f3762f801c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-03-24 15:08:24 +0100

    [web] move workflow related uicfg stuff to the workflow module
    --HG--
    branch : stable

--- web/views/actions.py
+++ web/views/actions.py
@@ -290,6 +290,7 @@ class AddRelatedActions(action.Action):
                         continue
                     # check the target types can be added as well
                     if teschema.may_have_permission('add', req):
+                        print 'hop', rschema, teschema, role
                         yield rschema, teschema, role
 
     def linkto_url(self, entity, rtype, etype, target):
@@ -404,16 +405,8 @@ class PoweredByAction(action.Action):
 
 addmenu = uicfg.actionbox_appearsin_addmenu
 addmenu.tag_subject_of(('*', 'require_permission', '*'), False)
-addmenu.tag_subject_of(('*', 'wf_info_for', '*'), False)
-addmenu.tag_object_of(('*', 'wf_info_for', '*'), False)
-addmenu.tag_object_of(('*', 'state_of', 'CWEType'), True)
-addmenu.tag_object_of(('*', 'transition_of', 'CWEType'), True)
 addmenu.tag_object_of(('*', 'relation_type', 'CWRType'), True)
 addmenu.tag_object_of(('*', 'from_entity', 'CWEType'), False)
 addmenu.tag_object_of(('*', 'to_entity', 'CWEType'), False)
 addmenu.tag_object_of(('*', 'in_group', 'CWGroup'), True)
 addmenu.tag_object_of(('*', 'bookmarked_by', 'CWUser'), True)
-addmenu.tag_subject_of(('Transition', 'destination_state', '*'), True)
-addmenu.tag_object_of(('*', 'allowed_transition', 'Transition'), True)
-addmenu.tag_object_of(('*', 'destination_state', 'State'), True)
-addmenu.tag_subject_of(('State', 'allowed_transition', '*'), True)
