5e0b8d57c65504a1ee5185f1b77065cea22254a3
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-03-31 12:26:07 +0200

    [testlib] simple helper method to register temporary appobjects (hooks for instance) with a context manager
    --HG--
    branch : stable

--- devtools/testlib.py
+++ devtools/testlib.py
@@ -12,6 +12,7 @@ import sys
 import re
 from urllib import unquote
 from math import log
+from contextlib import contextmanager
 
 import simplejson
 
@@ -353,6 +354,15 @@ class CubicWebTC(TestCase):
 
     def entity(self, rql, args=None, eidkey=None, req=None):
         return self.execute(rql, args, eidkey, req=req).get_entity(0, 0)
+
+    @contextmanager
+    def temporary_appobjects(self, *appobjects):
+        self.vreg._loadedmods.setdefault(self.__module__, {})
+        for obj in appobjects:
+            self.vreg.register(obj)
+        yield
+        for obj in appobjects:
+            self.vreg.unregister(obj)
 
     # vregistry inspection utilities ###########################################
 
