69c00f5c9d43df73e54241ffc26fbff7a11458bf
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-05-26 12:31:45 +0200

    [migration] force 3.8.3 migration, and fix it (same_as not necessarily there, missing group change for max-post-length)
    --HG--
    branch : stable

--- misc/migration/3.8.3_Any.py
+++ misc/migration/3.8.3_Any.py
@@ -1,2 +1,3 @@
-sync_schema_props_perms('same_as', syncperms=False)
+if 'same_as' in schema:
+    sync_schema_props_perms('same_as', syncperms=False)
 sync_schema_props_perms('Bookmark', syncperms=False)
