3243578f8815b39e7adf891634edf4b9b5a3a68c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-06-10 16:57:00 +0200

    [idownloadable] depends on a new mtconverter release

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -41,7 +41,7 @@ classifiers = [
 
 __depends__ = {
     'logilab-common': '>= 0.50.2',
-    'logilab-mtconverter': '>= 0.6.0',
+    'logilab-mtconverter': '>= 0.8.0',
     'rql': '>= 0.26.2',
     'yams': '>= 0.30.0',
     'docutils': '>= 0.6',
