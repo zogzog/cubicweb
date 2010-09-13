aee6c7629e1d296dadd795298eb3a0a63c439ae1
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2010-09-13 10:01:43 +0200

    [javascript] get rid of tabs
    --HG--
    branch : stable

--- web/data/cubicweb.image.js
+++ web/data/cubicweb.image.js
@@ -1,4 +1,3 @@
-
 jQuery.fn.autoResize = function() {
     // remove enforced with / height (by CSS and/or HTML attributes)
     this.css("width", "auto").css("height", "auto");
@@ -10,23 +9,23 @@ jQuery.fn.autoResize = function() {
     // we don't mind if content in [content]footer moved out of the screen
     var maxVSize = $(window).height() - ($(document).height() - imgVSize) + $('#footer').height() + $('#contentfooter').height();
     if (maxHSize > 0 && maxVSize > 0) {
-	// if image don't fit screen, set width or height so that
-	// browser keep img ratio, ensuring the other dimension will
-	// also fit the screen
-	if (imgHSize > maxHSize && ((imgVSize / imgHSize) * maxHSize) <= maxVSize) {
-	    this.css("width", maxHSize);
-	} else if (imgVSize > maxVSize && ((imgHSize / imgVSize) * maxVSize) <= maxHSize) {
-	    this.css("height", maxVSize);
-	}
-	else {
-	    // image already fit in screen, don't scale it up
-	}
+        // if image don't fit screen, set width or height so that
+        // browser keep img ratio, ensuring the other dimension will
+        // also fit the screen
+        if (imgHSize > maxHSize && ((imgVSize / imgHSize) * maxHSize) <= maxVSize) {
+            this.css("width", maxHSize);
+        } else if (imgVSize > maxVSize && ((imgHSize / imgVSize) * maxVSize) <= maxHSize) {
+            this.css("height", maxVSize);
+        }
+        else {
+            // image already fit in screen, don't scale it up
+        }
     } else {
-	// can't fit image in, don't do anything
+        // can't fit image in, don't do anything
     }
 };
 
 
 $(document).ready(function() {
-	$("img.contentimage").load(function() {$(this).autoResize()});
-    });
+        $("img.contentimage").load(function() {$(this).autoResize()});
+});
