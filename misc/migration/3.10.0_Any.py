e1b02ec7161ccb42e837bf17c69b092a2a91ee9b
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-10-01 15:45:38 +0200

    [3.10 migration] fix migration of cwproperties related to components/boxes

--- misc/migration/3.10.0_Any.py
+++ misc/migration/3.10.0_Any.py
@@ -2,4 +2,5 @@
 for x in rql('Any X,XK WHERE X pkey XK, '
              'X pkey ~= "boxes.%s" OR '
              'X pkey ~= "contentnavigation.%s"').entities():
-    x.set_attributes(pkey=u'ctxcomponents.' + x.pkey.split('.',1))
+    x.set_attributes(pkey=u'ctxcomponents.' + x.pkey.split('.', 1)[1])
+
