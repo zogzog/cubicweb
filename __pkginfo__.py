97feebfe2fa88302f11373c88c96f9003f26d479
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-10-21 09:34:21 +0200

    3.9.9
    --HG--
    branch : stable

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -22,7 +22,7 @@ software
 
 modname = distname = "cubicweb"
 
-numversion = (3, 9, 8)
+numversion = (3, 9, 9)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
