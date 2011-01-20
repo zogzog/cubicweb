90fc39d2e660397bbbabae9c1adac3ba8ff2a8e2
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-01-20 10:10:22 +0100

    [url publishing] evaluate url rewrite rules before rest path so one may easily override schema based urls

--- web/views/urlpublishing.py
+++ web/views/urlpublishing.py
@@ -156,7 +156,7 @@ class RestPathEvaluator(URLPathEvaluator):
 
         <etype>[[/<attribute name>]/<attribute value>]*
     """
-    priority = 2
+    priority = 3
 
     def evaluate_path(self, req, parts):
         if not (0 < len(parts) < 4):
@@ -214,7 +214,8 @@ class URLRewriteEvaluator(URLPathEvaluator):
 
     URL rewrite rule definitions are stored in URLRewriter objects
     """
-    priority = 3
+    priority = 2
+
     def evaluate_path(self, req, parts):
         # uri <=> req._twreq.path or req._twreq.uri
         uri = req.url_unquote('/' + '/'.join(parts))
@@ -236,6 +237,7 @@ class ActionPathEvaluator(URLPathEvaluator):
     <any evaluator path>/<action>
     """
     priority = 4
+
     def evaluate_path(self, req, parts):
         if len(parts) < 2:
             raise PathDontMatch()
