b814ba116d0428d7d052143d01f332323f0c8507
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-02-08 14:16:41 +0100

    [test, ui] mark CWSourceSchemaConfig as an internal type

--- schema.py
+++ schema.py
@@ -1,4 +1,4 @@
-# copyright 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2003-2011 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
 # This file is part of CubicWeb.
@@ -83,8 +83,7 @@ WORKFLOW_TYPES = set(('Transition', 'State', 'TrInfo', 'Workflow',
                       'SubWorkflowExitPoint'))
 
 INTERNAL_TYPES = set(('CWProperty', 'CWPermission', 'CWCache', 'ExternalUri',
-                      'CWSource', 'CWSourceHostConfig',
-))
+                      'CWSource', 'CWSourceHostConfig', 'CWSourceSchemaConfig'))
 
 
 _LOGGER = getLogger('cubicweb.schemaloader')
