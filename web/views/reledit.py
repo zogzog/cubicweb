f7c0edb59beef7b28be2404364dbfba3bc8f8d4d
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-03-31 15:18:21 +0200

    [reledit] fix crash due to wrong attributes usage
    --HG--
    branch : stable

--- web/views/reledit.py
+++ web/views/reledit.py
@@ -1,4 +1,4 @@
-# copyright 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2003-2011 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
 # This file is part of CubicWeb.
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with CubicWeb.  If not, see <http://www.gnu.org/licenses/>.
-"""the 'reedit' feature (eg edit attribute/relation from primary view"""
+"""the 'reledit' feature (eg edit attribute/relation from primary view)"""
 
 __docformat__ = "restructuredtext en"
 _ = unicode
@@ -221,10 +221,10 @@ class ClickAndEditFormView(EntityView):
         return self._build_zone(self._editzone, self._editzonemsg, self._editlogo)
 
     def _build_delete_zone(self):
-        return self._build_zone(self._deletezone, self._deletezonemsg, self._deletelogo)
+        return self._build_zone(self._deletezone, self._deletemsg, self._deletelogo)
 
     def _build_add_zone(self):
-        return self._build_zone(self._addzone, self._addzonemsg, self._addlogo)
+        return self._build_zone(self._addzone, self._addmsg, self._addlogo)
 
     def _build_divid(self, rtype, role, entity_eid):
         """ builds an id for the root div of a reledit widget """
