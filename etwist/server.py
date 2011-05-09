550a4b6319442b96342c273d9c322bee18d1c8a9
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-05-09 16:11:00 +0200

    [etwist] fix bug w/ new resources concantenation and https-url enabled sites

--- etwist/server.py
+++ etwist/server.py
@@ -1,4 +1,4 @@
-# copyright 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2003-2011 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
 # This file is part of CubicWeb.
@@ -99,8 +99,11 @@ class DataLookupDirectory(NoListingFile):
 
     def getChild(self, path, request):
         if not path:
-            if request.uri.startswith(self.data_modconcat_basepath):
-                resource_relpath = request.uri[len(self.data_modconcat_basepath):]
+            uri = request.uri
+            if uri.startswith('/https/'):
+                uri = uri[6:]
+            if uri.startswith(self.data_modconcat_basepath):
+                resource_relpath = uri[len(self.data_modconcat_basepath):]
                 if resource_relpath:
                     paths = resource_relpath.split(',')
                     try:
