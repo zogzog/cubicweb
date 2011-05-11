d8fd82298cad374e77eb7e6a32f75e02b13fbcf4
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-05-11 12:11:51 +0200

    missing future import

--- server/test/unittest_session.py
+++ server/test/unittest_session.py
@@ -15,6 +15,8 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with CubicWeb.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import with_statement
+
 from logilab.common.testlib import TestCase, unittest_main, mock_object
 
 from cubicweb.devtools.testlib import CubicWebTC
