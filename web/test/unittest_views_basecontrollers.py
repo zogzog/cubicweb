c6684205dd2effdfccd62589da686d1af2ad9bd6
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-05-26 08:57:15 +0200

    [test] fix failure introduced by yams'changeset 644:f9c79356b758

--- web/test/unittest_views_basecontrollers.py
+++ web/test/unittest_views_basecontrollers.py
@@ -238,7 +238,7 @@ class EditControllerTC(CubicWebTC):
                 }
         with self.assertRaises(ValidationError) as cm:
             self.ctrl_publish(req)
-        self.assertEqual(cm.exception.errors, {'amount-subject': 'value must be >= 0'})
+        self.assertEqual(cm.exception.errors, {'amount-subject': 'value -10 must be >= 0'})
         req = self.request(rollbackfirst=True)
         req.form = {'eid': ['X'],
                     '__type:X': 'Salesterm',
@@ -248,7 +248,7 @@ class EditControllerTC(CubicWebTC):
                     }
         with self.assertRaises(ValidationError) as cm:
             self.ctrl_publish(req)
-        self.assertEqual(cm.exception.errors, {'amount-subject': 'value must be <= 100'})
+        self.assertEqual(cm.exception.errors, {'amount-subject': 'value 110 must be <= 100'})
         req = self.request(rollbackfirst=True)
         req.form = {'eid': ['X'],
                     '__type:X': 'Salesterm',
