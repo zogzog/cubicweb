a1a4a02bfa673df14b0af5c238cd90d060bed25a
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-07-20 17:16:28 +0200

    3.13.2

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -22,7 +22,7 @@ software
 
 modname = distname = "cubicweb"
 
-numversion = (3, 13, 1)
+numversion = (3, 13, 2)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
