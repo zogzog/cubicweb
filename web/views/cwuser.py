b5a7511cd1b96adfb92eac35b10e63793702305c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-08-04 15:22:26 +0200

    [web] fix user management view selector
    --HG--
    branch : stable

--- web/views/cwuser.py
+++ web/views/cwuser.py
@@ -171,7 +171,7 @@ class CWUserManagementView(StartupView):
     # XXX one could wish to display for instance only user's firstname/surname
     # for non managers but filtering out NULL cause crash with an ldapuser
     # source.
-    __select__ = match_user_groups('managers')
+    __select__ = StartupView.__select__ & match_user_groups('managers')
     rql = ('Any U,USN,F,S,U,UAA,UDS, L,UAA,UDSN ORDERBY L WHERE U is CWUser, '
            'U login L, U firstname F, U surname S, '
            'U in_state US, US name USN, '
