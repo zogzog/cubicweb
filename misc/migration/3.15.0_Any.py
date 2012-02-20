c7a84e0c39343bd8da730647f9caf1dc48b798ac
Sylvain Thénault <sylvain.thenault@logilab.fr>
2012-02-20 16:13:36 +0100

    [ldap migration] use unicode strings

--- misc/migration/3.15.0_Any.py
+++ misc/migration/3.15.0_Any.py
@@ -2,9 +2,9 @@ sync_schema_props_perms('EmailAddress')
 
 for source in rql('CWSource X WHERE X type "ldapuser"').entities():
     config = source.dictconfig
-    host = config.pop('host', 'ldap')
-    protocol = config.pop('protocol', 'ldap')
-    source.set_attributes(url='%s://%s' % (protocol, host))
+    host = config.pop('host', u'ldap')
+    protocol = config.pop('protocol', u'ldap')
+    source.set_attributes(url=u'%s://%s' % (protocol, host))
     source.update_config(skip_unknown=True, **config)
 
 commit()
