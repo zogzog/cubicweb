57bfb347436b8eb014bc6d4e28caccd0ed4efb31
Aurelien Campeas <aurelien.campeas@logilab.fr>
2012-02-24 12:44:28 +0100

    [migration/3.14.4] use the right API else we break sqlserver instances (closes #2211248)
    --HG--
    branch : stable

--- misc/migration/3.14.4_Any.py
+++ misc/migration/3.14.4_Any.py
@@ -4,6 +4,7 @@ dbhelper = repo.system_source.dbhelper
 rdefdef = schema['CWSource'].rdef('name')
 attrtype = y2sql.type_from_constraints(dbhelper, rdefdef.object, rdefdef.constraints).split()[0]
 
-sql(dbhelper.sql_change_col_type('entities', 'asource', attrtype, False))
-sql(dbhelper.sql_change_col_type('entities', 'source', attrtype, False))
-sql(dbhelper.sql_change_col_type('deleted_entities', 'source', attrtype, False))
+cursor = session.cnxset['system']
+dbhelper.change_col_type(cursor, 'entities', 'asource', attrtype, False)
+dbhelper.change_col_type(cursor, 'entities', 'source', attrtype, False)
+dbhelper.change_col_type(cursor, 'deleted_entities', 'source', attrtype, False)
