d4b5b87bf93286a206537dc4575a192b52f79e11
Sylvain Thénault <sylvain.thenault@logilab.fr>
2012-04-10 21:52:23 +0200

    [3.15 migration] repo-uri is a c-c command option and does not need migration since it doesn't exist in the configuration file

--- misc/migration/3.15.0_common.py
+++ misc/migration/3.15.0_common.py
@@ -1,5 +1,2 @@
 undo_actions = config.cfgfile_parser.get('MAIN', 'undo-support', False)
 config.global_set_option('undo-enabled', bool(undo_actions))
-pyro_actions = config.cfgfile_parser.get('REMOTE', 'pyro', False)
-if pyro_actions:
-    config.global_set_option('repo-uri', 'pyro://')
