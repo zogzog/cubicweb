78886c1053b5de065ad563f7cd3a2a327d9c4dd1
Sylvain Thénault <sylvain.thenault@logilab.fr>
2012-05-15 10:36:02 +0200

    [data import] allow a request to be given as argument, ease use from web ui
    --HG--
    branch : stable

--- dataimport.py
+++ dataimport.py
@@ -446,9 +446,12 @@ class RQLObjectStore(ObjectStore):
         if session is None:
             sys.exit('please provide a session of run this script with cubicweb-ctl shell and pass cnx as session')
         if not hasattr(session, 'set_cnxset'):
-            # connection
-            cnx = session
-            session = session.request()
+            if hasattr(session, 'request'):
+                # connection object
+                cnx = session
+                session = session.request()
+            else: # object is already a request
+                cnx = session.cnx
             session.set_cnxset = lambda : None
             commit = commit or cnx.commit
         else:
