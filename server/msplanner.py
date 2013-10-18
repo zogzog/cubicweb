693fe545d48e91c04e02067c3c2411c96c0ddef3
Aurelien Campeas <aurelien.campeas@logilab.fr>
2013-10-18 16:58:48 +0200

    [server] deprecate the old multi-source system
    It is removed in 3.19.
    Closes #2919299

--- server/msplanner.py
+++ server/msplanner.py
@@ -92,6 +92,7 @@ from itertools import imap, ifilterfalse
 
 from logilab.common.compat import any
 from logilab.common.decorators import cached
+from logilab.common.deprecation import deprecated
 
 from rql import BadRQLQuery
 from rql.stmts import Union, Select
@@ -1262,6 +1263,7 @@ class PartPlanInformation(object):
         inputmap.update(step.outputmap)
 
 
+@deprecated('[3.18] old multi-source system will go away in the next version')
 class MSPlanner(SSPlanner):
     """MultiSourcesPlanner: build execution plan for rql queries
 
