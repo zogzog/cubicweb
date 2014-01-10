789713c5ad3c2cae4a0ace7dcb5b4451ff8ce777
Julien Cristau <julien.cristau@logilab.fr>
2014-01-10 15:02:07 +0100

    [server] remove old backwards compat for PropagateSubjectRelation*Hook
    Those classes were removed from server/hook.py.

--- server/hooksmanager.py
+++ server/hooksmanager.py
@@ -17,11 +17,6 @@
 # with CubicWeb.  If not, see <http://www.gnu.org/licenses/>.
 from logilab.common.deprecation import class_renamed, class_moved
 from cubicweb.server import hook
+
 SystemHook = class_renamed('SystemHook', hook.Hook)
-PropagateSubjectRelationHook = class_renamed('PropagateSubjectRelationHook',
-                                             hook.PropagateSubjectRelationHook)
-PropagateSubjectRelationAddHook = class_renamed('PropagateSubjectRelationAddHook',
-                                                hook.PropagateSubjectRelationAddHook)
-PropagateSubjectRelationDelHook = class_renamed('PropagateSubjectRelationDelHook',
-                                                hook.PropagateSubjectRelationDelHook)
 Hook = class_moved(hook.Hook)
