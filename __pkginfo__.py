e373d9d53642d01fbe2cf464df4c9a37bbeb42e2
Julien Cristau <julien.cristau@logilab.fr>
2014-02-14 16:23:46 +0100

    [pkg] prepare 3.18.3

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -22,14 +22,13 @@ software
 
 modname = distname = "cubicweb"
 
-numversion = (3, 18, 2)
+numversion = (3, 18, 3)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
 author = "Logilab"
 author_email = "contact@logilab.fr"
 web = 'http://www.cubicweb.org'
-ftp = 'ftp://ftp.logilab.org/pub/cubicweb'
 license = 'LGPL'
 
 classifiers = [
