0ef819c4715f768679778d471896ed0ba06baf6d
Laura Médioni <laura.medioni@logilab.fr>
2014-03-24 16:38:57 +0100

    [server] Fix AttributeError in extid2eid
    Session._tx was renamed in cb87e831c183, use _cnx instead.

--- server/repository.py
+++ server/repository.py
@@ -1095,7 +1095,7 @@ class Repository(object):
                     hook.CleanupDeletedEidsCacheOp.get_instance(session).add_data(entity.eid)
                     self.system_source.delete_info_multi(session, [entity])
                     if source.should_call_hooks:
-                        session._tx.pending_operations = pending_operations
+                        session._cnx.pending_operations = pending_operations
             raise
 
     def add_info(self, session, entity, source, extid=None, complete=True):
