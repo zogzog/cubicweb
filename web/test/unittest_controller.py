d9d95b869e5ad3a4be5ff3c37aaf1d7732b5f4f9
Julien Cristau <julien.cristau@logilab.fr>
2014-04-02 17:03:46 +0200

    [web/test] move unittest_controller to RepoAccess test API

--- web/test/unittest_controller.py
+++ web/test/unittest_controller.py
@@ -28,30 +28,32 @@ from cubicweb.devtools import testlib
 class BaseControllerTC(testlib.CubicWebTC):
 
     def test_parse_datetime_ok(self):
-        ctrl = self.vreg['controllers'].select('view', self.request())
-        pd = ctrl._cw.parse_datetime
-        self.assertIsInstance(pd('2006/06/24 12:18'), datetime)
-        self.assertIsInstance(pd('2006/06/24'), date)
-        self.assertIsInstance(pd('2006/06/24 12:18', 'Datetime'), datetime)
-        self.assertIsInstance(pd('2006/06/24', 'Datetime'), datetime)
-        self.assertIsInstance(pd('2006/06/24', 'Date'), date)
-        self.assertIsInstance(pd('12:18', 'Time'), time)
+        with self.admin_access.web_request() as req:
+            ctrl = self.vreg['controllers'].select('view', req)
+            pd = ctrl._cw.parse_datetime
+            self.assertIsInstance(pd('2006/06/24 12:18'), datetime)
+            self.assertIsInstance(pd('2006/06/24'), date)
+            self.assertIsInstance(pd('2006/06/24 12:18', 'Datetime'), datetime)
+            self.assertIsInstance(pd('2006/06/24', 'Datetime'), datetime)
+            self.assertIsInstance(pd('2006/06/24', 'Date'), date)
+            self.assertIsInstance(pd('12:18', 'Time'), time)
 
     def test_parse_datetime_ko(self):
-        ctrl = self.vreg['controllers'].select('view', self.request())
-        pd = ctrl._cw.parse_datetime
-        self.assertRaises(ValueError,
-                          pd, '2006/06/24 12:188', 'Datetime')
-        self.assertRaises(ValueError,
-                          pd, '2006/06/240', 'Datetime')
-        self.assertRaises(ValueError,
-                          pd, '2006/06/24 12:18', 'Date')
-        self.assertRaises(ValueError,
-                          pd, '2006/24/06', 'Date')
-        self.assertRaises(ValueError,
-                          pd, '2006/06/240', 'Date')
-        self.assertRaises(ValueError,
-                          pd, '12:188', 'Time')
+        with self.admin_access.web_request() as req:
+            ctrl = self.vreg['controllers'].select('view', req)
+            pd = ctrl._cw.parse_datetime
+            self.assertRaises(ValueError,
+                              pd, '2006/06/24 12:188', 'Datetime')
+            self.assertRaises(ValueError,
+                              pd, '2006/06/240', 'Datetime')
+            self.assertRaises(ValueError,
+                              pd, '2006/06/24 12:18', 'Date')
+            self.assertRaises(ValueError,
+                              pd, '2006/24/06', 'Date')
+            self.assertRaises(ValueError,
+                              pd, '2006/06/240', 'Date')
+            self.assertRaises(ValueError,
+                              pd, '12:188', 'Time')
 
 if __name__ == '__main__':
     unittest_main()
