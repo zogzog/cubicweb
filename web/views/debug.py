58fef838533a4f584bcfe63a9edb49508e401c95
Julien Cristau <julien.cristau@logilab.fr>
2014-04-09 10:51:15 +0200

    [web/debug] remove code broken by session refactoring
    session.cnx and cnx.check() no longer exist.

--- web/views/debug.py
+++ web/views/debug.py
@@ -131,19 +131,9 @@ class ProcessInformationView(StartupView):
             sessions = SESSION_MANAGER.current_sessions()
             w(u'<h3>%s</h3>' % _('opened web sessions'))
             if sessions:
-                n_no_cnx_sessions = 0
                 w(u'<ul>')
                 for session in sessions:
-                    if not session.cnx:
-                        # We do not want to list all sessions without cnx
-                        # Their session ID are useless, hence we just count them
-                        n_no_cnx_sessions += 1
-                        continue
-                    try:
-                        last_usage_time = session.cnx.check()
-                    except BadConnectionId:
-                        w(u'<li>%s (INVALID)</li>' % session.sessionid)
-                        continue
+                    last_usage_time = session.mtime
                     w(u'<li>%s (%s: %s)<br/>' % (
                         session.sessionid,
                         _('last usage'),
@@ -151,9 +141,6 @@ class ProcessInformationView(StartupView):
                     dict_to_html(w, session.data)
                     w(u'</li>')
                 w(u'</ul>')
-                if n_no_cnx_sessions > 0:
-                    w(u'<h3>%s %s</h3>' % (n_no_cnx_sessions,
-                                           _('web sessions without CNX')))
             else:
                 w(u'<p>%s</p>' % _('no web sessions found'))
 
