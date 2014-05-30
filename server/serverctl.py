1b496d50f5239cc3bce66d8fda8f37552a9cb315
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-05-30 17:46:40 +0200

    [tests/checkintegrity] use the new connection api
    * serverctl: the db-check command also uses an internal connection
      object, which makes more sense in the context of `checkintegrity`.
    
    * checkintegrity: rename srvcnx to just cnx (a bit because in the near
      future, there will be one cnx to rule them all)

--- server/serverctl.py
+++ server/serverctl.py
@@ -1,4 +1,4 @@
-# copyright 2003-2012 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2003-2014 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
 # This file is part of CubicWeb.
@@ -979,10 +979,12 @@ option is set to "y" or "yes" (may be long for large database).'}
         appid = args[0]
         config = ServerConfiguration.config_for(appid)
         config.repairing = self.config.force
-        repo, cnx = repo_cnx(config)
-        with cnx:
+        repo, _cnx = repo_cnx(config)
+        with repo.internal_cnx() as cnx:
             check(repo, cnx,
-                  self.config.checks, self.config.reindex, self.config.autofix)
+                  self.config.checks,
+                  self.config.reindex,
+                  self.config.autofix)
 
 
 class RebuildFTICommand(Command):
