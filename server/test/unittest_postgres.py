c3ea434197f34926f9a85d821b9c651df24c0a08
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-06-02 13:09:14 +0200

    [tests/postgres] use the new connection api

--- server/test/unittest_postgres.py
+++ server/test/unittest_postgres.py
@@ -52,16 +52,17 @@ class PostgresFTITC(CubicWebTC):
         self.assertEqual(set(), set(range1) & set(range2))
 
     def test_occurence_count(self):
-        req = self.request()
-        c1 = req.create_entity('Card', title=u'c1',
-                               content=u'cubicweb cubicweb cubicweb')
-        c2 = req.create_entity('Card', title=u'c3',
-                               content=u'cubicweb')
-        c3 = req.create_entity('Card', title=u'c2',
-                               content=u'cubicweb cubicweb')
-        self.commit()
-        self.assertEqual(req.execute('Card X ORDERBY FTIRANK(X) DESC WHERE X has_text "cubicweb"').rows,
-                         [[c1.eid,], [c3.eid,], [c2.eid,]])
+        with self.admin_access.repo_cnx() as cnx:
+            c1 = cnx.create_entity('Card', title=u'c1',
+                                   content=u'cubicweb cubicweb cubicweb')
+            c2 = cnx.create_entity('Card', title=u'c3',
+                                   content=u'cubicweb')
+            c3 = cnx.create_entity('Card', title=u'c2',
+                                   content=u'cubicweb cubicweb')
+            cnx.commit()
+            self.assertEqual(cnx.execute('Card X ORDERBY FTIRANK(X) DESC '
+                                         'WHERE X has_text "cubicweb"').rows,
+                             [[c1.eid,], [c3.eid,], [c2.eid,]])
 
 
     def test_attr_weight(self):
@@ -69,43 +70,48 @@ class PostgresFTITC(CubicWebTC):
             __select__ = is_instance('Card')
             attr_weight = {'title': 'A'}
         with self.temporary_appobjects(CardIFTIndexableAdapter):
-            req = self.request()
-            c1 = req.create_entity('Card', title=u'c1',
-                                   content=u'cubicweb cubicweb cubicweb')
-            c2 = req.create_entity('Card', title=u'c2',
-                                   content=u'cubicweb cubicweb')
-            c3 = req.create_entity('Card', title=u'cubicweb',
-                                   content=u'autre chose')
-            self.commit()
-            self.assertEqual(req.execute('Card X ORDERBY FTIRANK(X) DESC WHERE X has_text "cubicweb"').rows,
-                             [[c3.eid,], [c1.eid,], [c2.eid,]])
+            with self.admin_access.repo_cnx() as cnx:
+                c1 = cnx.create_entity('Card', title=u'c1',
+                                       content=u'cubicweb cubicweb cubicweb')
+                c2 = cnx.create_entity('Card', title=u'c2',
+                                       content=u'cubicweb cubicweb')
+                c3 = cnx.create_entity('Card', title=u'cubicweb',
+                                       content=u'autre chose')
+                cnx.commit()
+                self.assertEqual(cnx.execute('Card X ORDERBY FTIRANK(X) DESC '
+                                             'WHERE X has_text "cubicweb"').rows,
+                                 [[c3.eid,], [c1.eid,], [c2.eid,]])
 
     def test_entity_weight(self):
         class PersonneIFTIndexableAdapter(IFTIndexableAdapter):
             __select__ = is_instance('Personne')
             entity_weight = 2.0
         with self.temporary_appobjects(PersonneIFTIndexableAdapter):
-            req = self.request()
-            c1 = req.create_entity('Personne', nom=u'c1', prenom=u'cubicweb')
-            c2 = req.create_entity('Comment', content=u'cubicweb cubicweb', comments=c1)
-            c3 = req.create_entity('Comment', content=u'cubicweb cubicweb cubicweb', comments=c1)
-            self.commit()
-            self.assertEqual(req.execute('Any X ORDERBY FTIRANK(X) DESC WHERE X has_text "cubicweb"').rows,
-                              [[c1.eid,], [c3.eid,], [c2.eid,]])
+            with self.admin_access.repo_cnx() as cnx:
+                c1 = cnx.create_entity('Personne', nom=u'c1', prenom=u'cubicweb')
+                c2 = cnx.create_entity('Comment', content=u'cubicweb cubicweb',
+                                       comments=c1)
+                c3 = cnx.create_entity('Comment', content=u'cubicweb cubicweb cubicweb',
+                                       comments=c1)
+                cnx.commit()
+                self.assertEqual(cnx.execute('Any X ORDERBY FTIRANK(X) DESC '
+                                             'WHERE X has_text "cubicweb"').rows,
+                                  [[c1.eid,], [c3.eid,], [c2.eid,]])
 
 
     def test_tz_datetime(self):
-        self.execute("INSERT Personne X: X nom 'bob', X tzdatenaiss %(date)s",
-                     {'date': datetime(1977, 6, 7, 2, 0, tzinfo=FixedOffset(1))})
-        datenaiss = self.execute("Any XD WHERE X nom 'bob', X tzdatenaiss XD")[0][0]
-        self.assertEqual(datenaiss.tzinfo, None)
-        self.assertEqual(datenaiss.utctimetuple()[:5], (1977, 6, 7, 1, 0))
-        self.commit()
-        self.execute("INSERT Personne X: X nom 'boby', X tzdatenaiss %(date)s",
-                     {'date': datetime(1977, 6, 7, 2, 0)})
-        datenaiss = self.execute("Any XD WHERE X nom 'boby', X tzdatenaiss XD")[0][0]
-        self.assertEqual(datenaiss.tzinfo, None)
-        self.assertEqual(datenaiss.utctimetuple()[:5], (1977, 6, 7, 2, 0))
+        with self.admin_access.repo_cnx() as cnx:
+            cnx.execute("INSERT Personne X: X nom 'bob', X tzdatenaiss %(date)s",
+                        {'date': datetime(1977, 6, 7, 2, 0, tzinfo=FixedOffset(1))})
+            datenaiss = cnx.execute("Any XD WHERE X nom 'bob', X tzdatenaiss XD")[0][0]
+            self.assertEqual(datenaiss.tzinfo, None)
+            self.assertEqual(datenaiss.utctimetuple()[:5], (1977, 6, 7, 1, 0))
+            cnx.commit()
+            cnx.execute("INSERT Personne X: X nom 'boby', X tzdatenaiss %(date)s",
+                        {'date': datetime(1977, 6, 7, 2, 0)})
+            datenaiss = cnx.execute("Any XD WHERE X nom 'boby', X tzdatenaiss XD")[0][0]
+            self.assertEqual(datenaiss.tzinfo, None)
+            self.assertEqual(datenaiss.utctimetuple()[:5], (1977, 6, 7, 2, 0))
 
 if __name__ == '__main__':
     from logilab.common.testlib import unittest_main
