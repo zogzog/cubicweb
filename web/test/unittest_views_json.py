94425a00f336f8049a2510d77beee6c9e3f8fdae
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-06-02 17:21:52 +0200

    [webtests/views_json] use the new connection api

--- web/test/unittest_views_json.py
+++ web/test/unittest_views_json.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# copyright 2012 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2014 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 #
 # This file is part of CubicWeb.
@@ -18,9 +18,6 @@
 # with CubicWeb.  If not, see <http://www.gnu.org/licenses/>.
 from cubicweb.devtools.testlib import CubicWebTC
 
-from cubicweb.utils import json
-
-from cubicweb.web.application import anonymized_request
 
 class JsonViewsTC(CubicWebTC):
     anonymize = True
@@ -31,47 +28,47 @@ class JsonViewsTC(CubicWebTC):
         self.config.global_set_option('anonymize-jsonp-queries', self.anonymize)
 
     def test_json_rsetexport(self):
-        req = self.request()
-        rset = req.execute('Any GN,COUNT(X) GROUPBY GN ORDERBY GN WHERE X in_group G, G name GN')
-        data = self.view('jsonexport', rset)
-        self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
-        self.assertListEqual(data, [["guests", 1], ["managers", 1]])
+        with self.admin_access.web_request() as req:
+            rset = req.execute('Any GN,COUNT(X) GROUPBY GN ORDERBY GN WHERE X in_group G, G name GN')
+            data = self.view('jsonexport', rset)
+            self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
+            self.assertListEqual(data, [["guests", 1], ["managers", 1]])
 
     def test_json_rsetexport_empty_rset(self):
-        req = self.request()
-        rset = req.execute('Any X WHERE X is CWUser, X login "foobarbaz"')
-        data = self.view('jsonexport', rset)
-        self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
-        self.assertListEqual(data, [])
+        with self.admin_access.web_request() as req:
+            rset = req.execute('Any X WHERE X is CWUser, X login "foobarbaz"')
+            data = self.view('jsonexport', rset)
+            self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
+            self.assertListEqual(data, [])
 
     def test_json_rsetexport_with_jsonp(self):
-        req = self.request()
-        req.form.update({'callback': 'foo',
-                         'rql': 'Any GN,COUNT(X) GROUPBY GN ORDERBY GN WHERE X in_group G, G name GN',
-                         })
-        data = self.ctrl_publish(req, ctrl='jsonp')
-        self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/javascript'])
-        # because jsonp anonymizes data, only 'guests' group should be found
-        self.assertEqual(data, 'foo(%s)' % self.res_jsonp_data)
+        with self.admin_access.web_request() as req:
+            req.form.update({'callback': 'foo',
+                             'rql': 'Any GN,COUNT(X) GROUPBY GN ORDERBY GN '
+                             'WHERE X in_group G, G name GN'})
+            data = self.ctrl_publish(req, ctrl='jsonp')
+            self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/javascript'])
+            # because jsonp anonymizes data, only 'guests' group should be found
+            self.assertEqual(data, 'foo(%s)' % self.res_jsonp_data)
 
     def test_json_rsetexport_with_jsonp_and_bad_vid(self):
-        req = self.request()
-        req.form.update({'callback': 'foo',
-                         'vid': 'table', # <-- this parameter should be ignored by jsonp controller
-                         'rql': 'Any GN,COUNT(X) GROUPBY GN ORDERBY GN WHERE X in_group G, G name GN',
-                         })
-        data = self.ctrl_publish(req, ctrl='jsonp')
-        self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/javascript'])
-        # result should be plain json, not the table view
-        self.assertEqual(data, 'foo(%s)' % self.res_jsonp_data)
+        with self.admin_access.web_request() as req:
+            req.form.update({'callback': 'foo',
+                             'vid': 'table', # <-- this parameter should be ignored by jsonp controller
+                             'rql': 'Any GN,COUNT(X) GROUPBY GN ORDERBY GN '
+                             'WHERE X in_group G, G name GN'})
+            data = self.ctrl_publish(req, ctrl='jsonp')
+            self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/javascript'])
+            # result should be plain json, not the table view
+            self.assertEqual(data, 'foo(%s)' % self.res_jsonp_data)
 
     def test_json_ersetexport(self):
-        req = self.request()
-        rset = req.execute('Any G ORDERBY GN WHERE G is CWGroup, G name GN')
-        data = self.view('ejsonexport', rset)
-        self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
-        self.assertEqual(data[0]['name'], 'guests')
-        self.assertEqual(data[1]['name'], 'managers')
+        with self.admin_access.web_request() as req:
+            rset = req.execute('Any G ORDERBY GN WHERE G is CWGroup, G name GN')
+            data = self.view('ejsonexport', rset)
+            self.assertEqual(req.headers_out.getRawHeaders('content-type'), ['application/json'])
+            self.assertEqual(data[0]['name'], 'guests')
+            self.assertEqual(data[1]['name'], 'managers')
 
 
 class NotAnonymousJsonViewsTC(JsonViewsTC):
