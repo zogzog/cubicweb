a6077f84539c374ddba530d94fcc67ca57320c80
Christophe de Vienne <christophe@unlish.com>
2014-09-18 11:17:57 +0200

    Add the 'pyramid-session-secret' option to all-in-one.conf
    This value is used by pyramid_cubicweb to initialize the cookie factory.

--- site_cubicweb.py
+++ site_cubicweb.py
@@ -1,3 +1,15 @@
+from logilab.common.configuration import REQUIRED
+
 import cubicweb.schema
 
 cubicweb.schema.INTERNAL_TYPES.add('CWSession')
+
+options = (
+    ('pyramid-session-secret', {
+        'type': 'string',
+        'default': REQUIRED,
+        'help': 'Secret phrase to sign the session cookie',
+        'group': 'pyramid',
+        'level': 3
+    }),
+)
