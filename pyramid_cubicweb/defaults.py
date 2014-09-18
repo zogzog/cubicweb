9d49e2f28d82cf3c866c437963665ab3bd4dca1c
Christophe de Vienne <christophe@unlish.com>
2014-09-18 11:43:45 +0200

    Use AuthTktAuthenticationPolicy

--- pyramid_cubicweb/defaults.py
+++ pyramid_cubicweb/defaults.py
@@ -1,4 +1,6 @@
-from pyramid.authentication import SessionAuthenticationPolicy
+import warnings
+
+from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 
 from pyramid_cubicweb.core import get_principals
@@ -7,8 +9,26 @@ from pyramid_cubicweb.core import get_principals
 def includeme(config):
     config.include('pyramid_cubicweb.session')
 
+    secret = config.registry['cubicweb.config']['pyramid-auth-secret']
+
+    if not secret:
+        secret = 'notsosecret'
+        warnings.warn('''
+
+            !! WARNING !! !! WARNING !!
+
+            The authentication cookies are signed with a static secret key.
+            To put your own secret key, edit your all-in-one.conf file
+            and set the 'pyramid-session-secret' key.
+
+            YOU SHOULD STOP THIS INSTANCE unless your really know what you
+            are doing !!
+
+        ''')
+
     config.set_authentication_policy(
-        SessionAuthenticationPolicy(callback=get_principals))
+        AuthTktAuthenticationPolicy(
+            secret, callback=get_principals, hashalg='sha512'))
     config.set_authorization_policy(ACLAuthorizationPolicy())
 
     config.include('pyramid_cubicweb.login')
