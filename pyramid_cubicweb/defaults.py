6aaf0a8805b8db0fa5d1e0fc64d9ca8223e5d665
Christophe de Vienne <christophe@unlish.com>
2014-11-02 22:54:24 +0100

    [auth] Make last_login_time updated.
    The update is done when the user logs in, then every time the authentication is
    reissued.
    
    Closes #4549891

--- pyramid_cubicweb/defaults.py
+++ pyramid_cubicweb/defaults.py
@@ -1,9 +1,39 @@
+import datetime
+import logging
 import warnings
 
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 
 from pyramid_cubicweb.core import get_principals
+
+log = logging.getLogger(__name__)
+
+
+class CubicWebAuthTktAuthenticationPolicy(AuthTktAuthenticationPolicy):
+    """An authentication policy that update the user last_login_time.
+
+    The update is done in the 'remember' method, which is called on login,
+    and each time the authentication ticket is reissued.
+
+    Meaning, the last_login_time is updated reissue_time seconds (maximum)
+    before the last request by the user.
+    """
+
+    def remember(self, request, principal, **kw):
+        headers = super(CubicWebAuthTktAuthenticationPolicy, self).remember(
+            request, principal, **kw)
+        try:
+            repo = request.registry['cubicweb.repository']
+            with repo.internal_session() as cnx:
+                cnx.execute(
+                    "SET U last_login_time %(now)s WHERE U eid %(user)s", {
+                        'now': datetime.datetime.now(),
+                        'user': principal})
+                cnx.commit()
+        except:
+            log.exception("Failed to update last_login_time")
+        return headers
 
 
 def includeme(config):
@@ -27,7 +57,7 @@ def includeme(config):
         ''')
 
     config.set_authentication_policy(
-        AuthTktAuthenticationPolicy(
+        CubicWebAuthTktAuthenticationPolicy(
             secret, callback=get_principals, hashalg='sha512',
             reissue_time=3600))
     config.set_authorization_policy(ACLAuthorizationPolicy())
