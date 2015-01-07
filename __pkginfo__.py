3d84b5d76269c9e43e21f37370576b6339f806cc
Julien Cristau <julien.cristau@logilab.fr>
2015-01-07 14:38:25 +0100

    [pkg] 3.19.7

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -22,7 +22,7 @@ software
 
 modname = distname = "cubicweb"
 
-numversion = (3, 19, 6)
+numversion = (3, 19, 7)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
