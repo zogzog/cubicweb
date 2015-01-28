6f197123cc01b359b8f648d27b351610dda08d56
Julien Cristau <julien.cristau@logilab.fr>
2015-01-28 15:06:34 +0100

    [migration/3.20] sync rtypes before etypes
    We need to make sure the rtypes are inlined before adding unique indices.

--- misc/migration/3.20.0_Any.py
+++ misc/migration/3.20.0_Any.py
@@ -1,6 +1,6 @@
-sync_schema_props_perms('State')
 sync_schema_props_perms('state_of')
+sync_schema_props_perms('transition_of')
+sync_schema_props_perms('State')
 sync_schema_props_perms('BaseTransition')
 sync_schema_props_perms('Transition')
 sync_schema_props_perms('WorkflowTransition')
-sync_schema_props_perms('transition_of')
