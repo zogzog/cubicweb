fea2c4cbe6e711dcbe2ea698cc497a9de4ca097d
Julien Cristau <julien.cristau@logilab.fr>
2015-09-08 16:25:26 +0200

    [test] use unicode for rql queries (5/7)
    Related to #6694446

--- web/test/unittest_views_searchrestriction.py
+++ web/test/unittest_views_searchrestriction.py
@@ -37,62 +37,62 @@ class InsertAttrRelationTC(CubicWebTC):
 
     @property
     def select(self):
-        return self.parse('Any B,(NOW - CD),S,V,U,GROUP_CONCAT(TN),VN,P,CD,BMD '
-                          'GROUPBY B,CD,S,V,U,VN,P,BMD '
-                          'WHERE B in_state S, B creation_date CD, '
-                          'B modification_date BMD, T? tags B, T name TN, '
-                          'V? bookmarked_by B, V title VN, B created_by U?, '
-                          'B in_group P, P name "managers"')
+        return self.parse(u'Any B,(NOW - CD),S,V,U,GROUP_CONCAT(TN),VN,P,CD,BMD '
+                           'GROUPBY B,CD,S,V,U,VN,P,BMD '
+                           'WHERE B in_state S, B creation_date CD, '
+                           'B modification_date BMD, T? tags B, T name TN, '
+                           'V? bookmarked_by B, V title VN, B created_by U?, '
+                           'B in_group P, P name "managers"')
 
     def test_1(self):
         self.assertEqual(self._generate(self.select, 'in_state', 'subject', 'name'),
-                          "DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name 'managers', "
-                          "B in_state A, B is CWUser, A name C")
+                         'DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name "managers", '
+                         'B in_state A, B is CWUser, A name C')
 
     def test_2(self):
         self.assertEqual(self._generate(self.select, 'tags', 'object', 'name'),
-                          "DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name 'managers', "
-                          "A tags B, B is CWUser, A name C")
+                         'DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name "managers", '
+                         'A tags B, B is CWUser, A name C')
 
     def test_3(self):
         self.assertEqual(self._generate(self.select, 'created_by', 'subject', 'login'),
-                          "DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name 'managers', "
-                          "B created_by A, B is CWUser, A login C")
+                         'DISTINCT Any A,C ORDERBY C WHERE B in_group P, P name "managers", '
+                         'B created_by A, B is CWUser, A login C')
 
     def test_4(self):
-        self.assertEqual(self._generate(self.parse('Any X WHERE X is CWUser'), 'created_by', 'subject', 'login'),
+        self.assertEqual(self._generate(self.parse(u'Any X WHERE X is CWUser'), 'created_by', 'subject', 'login'),
                           "DISTINCT Any A,B ORDERBY B WHERE X is CWUser, X created_by A, A login B")
 
     def test_5(self):
-        self.assertEqual(self._generate(self.parse('Any X,L WHERE X is CWUser, X login L'), 'created_by', 'subject', 'login'),
+        self.assertEqual(self._generate(self.parse(u'Any X,L WHERE X is CWUser, X login L'), 'created_by', 'subject', 'login'),
                           "DISTINCT Any A,B ORDERBY B WHERE X is CWUser, X created_by A, A login B")
 
     def test_nonregr1(self):
-        select = self.parse('Any T,V WHERE T bookmarked_by V?, '
-                            'V in_state VS, VS name "published", T created_by U')
+        select = self.parse(u'Any T,V WHERE T bookmarked_by V?, '
+                             'V in_state VS, VS name "published", T created_by U')
         self.assertEqual(self._generate(select, 'created_by', 'subject', 'login'),
                           "DISTINCT Any A,B ORDERBY B WHERE T created_by U, "
                           "T created_by A, T is Bookmark, A login B")
 
     def test_nonregr2(self):
         #'DISTINCT Any X,TMP,N WHERE P name TMP, X version_of P, P is Project, X is Version, not X in_state S,S name "published", X num N ORDERBY TMP,N'
-        select = self.parse('DISTINCT Any V,TN,L ORDERBY TN,L WHERE T nom TN, V connait T, T is Personne, V is CWUser,'
-                            'NOT V in_state VS, VS name "published", V login L')
+        select = self.parse(u'DISTINCT Any V,TN,L ORDERBY TN,L WHERE T nom TN, V connait T, T is Personne, V is CWUser,'
+                             'NOT V in_state VS, VS name "published", V login L')
         rschema = self.schema['connait']
         for rdefs in rschema.rdefs.itervalues():
             rdefs.cardinality =  '++'
         try:
             self.assertEqual(self._generate(select, 'in_state', 'subject', 'name'),
-                              "DISTINCT Any A,B ORDERBY B WHERE V is CWUser, "
-                              "NOT EXISTS(V in_state VS), VS name 'published', "
-                              "V in_state A, A name B")
+                             'DISTINCT Any A,B ORDERBY B WHERE V is CWUser, '
+                             'NOT EXISTS(V in_state VS), VS name "published", '
+                             'V in_state A, A name B')
         finally:
             for rdefs in rschema.rdefs.itervalues():
                 rdefs.cardinality =  '**'
 
     def test_nonregr3(self):
         #'DISTINCT Any X,TMP,N WHERE P name TMP, X version_of P, P is Project, X is Version, not X in_state S,S name "published", X num N ORDERBY TMP,N'
-        select = self.parse('DISTINCT Any X, MAX(Y) GROUPBY X WHERE X is CWUser, Y is Bookmark, X in_group A')
+        select = self.parse(u'DISTINCT Any X, MAX(Y) GROUPBY X WHERE X is CWUser, Y is Bookmark, X in_group A')
         self.assertEqual(self._generate(select, 'in_group', 'subject', 'name'),
                           "DISTINCT Any B,C ORDERBY C WHERE X is CWUser, X in_group B, B name C")
 
