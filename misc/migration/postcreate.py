88a3d074467f75d0722bb06fb025ce1eaadec389
Julien Cristau <julien.cristau@logilab.fr>
2015-10-21 17:21:18 +0200

    [postcreate] properly set is_instance_of for bootstrap entities
    Entities created before CWEType itself need the is/is_instance_of
    relations created later.  We were actually only inserting "is".
    Closes #7656606.

--- misc/migration/postcreate.py
+++ misc/migration/postcreate.py
@@ -33,8 +33,8 @@ for rtype in ('is', 'is_instance_of'):
     sql('INSERT INTO %s_relation '
         'SELECT X.eid, ET.cw_eid FROM entities as X, cw_CWEType as ET '
         'WHERE X.type=ET.cw_name AND NOT EXISTS('
-        '      SELECT 1 from is_relation '
-        '      WHERE eid_from=X.eid AND eid_to=ET.cw_eid)' % rtype)
+        '      SELECT 1 from %s_relation '
+        '      WHERE eid_from=X.eid AND eid_to=ET.cw_eid)' % (rtype, rtype))
 
 # user workflow
 userwf = add_workflow(_('default user workflow'), 'CWUser')
