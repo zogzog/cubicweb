a79218b748b20c71984e7b1da01dfdbd25fd9cab
Samuel Trégouët <samuel.tregouet@logilab.fr>
2015-12-03 14:55:10 +0100

    [dataimport] prepare_insert_entity should not crash with SQLGenObjectStore

--- dataimport/test/test_pgstore.py
+++ dataimport/test/test_pgstore.py
@@ -24,6 +24,7 @@ from six import PY2
 from logilab.common.testlib import TestCase, unittest_main
 
 from cubicweb.dataimport import pgstore
+from cubicweb.devtools import testlib
 
 
 class CreateCopyFromBufferTC(TestCase):
@@ -89,6 +90,17 @@ class CreateCopyFromBufferTC(TestCase):
         results = pgstore._create_copyfrom_buffer(data, ('integer', 'text'))
         expected = u'''42\téléphant\n6\tbabar'''
         self.assertEqual(expected, results.getvalue())
+
+
+class SQLGenObjectStoreTC(testlib.CubicWebTC):
+
+    def test_prepare_insert_entity(self):
+        with self.admin_access.repo_cnx() as cnx:
+            store = pgstore.SQLGenObjectStore(cnx)
+            eid = store.prepare_insert_entity('CWUser', login=u'toto',
+                                              upassword=u'pwd')
+            self.assertIsNotNone(eid)
+
 
 if __name__ == '__main__':
     unittest_main()
