a8bda4967a1f9b3bfce99d7edbcd1f9a0c971b2d
Rémi Cardona <remi.cardona@logilab.fr>
2015-12-15 18:15:08 +0100

    [pkg] 3.21.5

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -22,7 +22,7 @@ software
 
 modname = distname = "cubicweb"
 
-numversion = (3, 21, 4)
+numversion = (3, 21, 5)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
