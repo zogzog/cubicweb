441700d83a7b57914ab324d1e9ed4723d7b9dec3
Sylvain Thénault <sylvain.thenault@logilab.fr>
2016-06-29 13:51:25 +0200

    prepare 3.23.1
    --HG--
    branch : 3.23

--- cubicweb/__pkginfo__.py
+++ cubicweb/__pkginfo__.py
@@ -27,7 +27,7 @@ import glob
 
 modname = distname = "cubicweb"
 
-numversion = (3, 23, 0)
+numversion = (3, 23, 1)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
