4427610419859efdc4639079524b602d7f648b93
Denis Laxalde <denis.laxalde@logilab.fr>
2016-09-12 09:03:07 +0200

    [devctl] Remove commented LiveServerCommand
    The code got commented in 2010 (changeset 5ab3f63b06ad)...

--- cubicweb/devtools/devctl.py
+++ cubicweb/devtools/devctl.py
@@ -512,20 +512,6 @@ def update_cube_catalogs(cubedir):
     return toedit
 
 
-# XXX totally broken, fix it
-# class LiveServerCommand(Command):
-#     """Run a server from within a cube directory.
-#     """
-#     name = 'live-server'
-#     arguments = ''
-#     options = ()
-
-#     def run(self, args):
-#         """run the command with its specific arguments"""
-#         from cubicweb.devtools.livetest import runserver
-#         runserver()
-
-
 class NewCubeCommand(Command):
     """Create a new cube.
 
@@ -852,7 +838,6 @@ class GenerateSchema(Command):
 
 for cmdcls in (UpdateCubicWebCatalogCommand,
                UpdateCubeCatalogCommand,
-               #LiveServerCommand,
                NewCubeCommand,
                ExamineLogCommand,
                GenerateSchema,
