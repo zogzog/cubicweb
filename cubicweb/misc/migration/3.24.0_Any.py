61ae55fc3e0bbcc2a0c58002bf0f6a26c6c295fc
Sylvain Thénault <sylvain.thenault@logilab.fr>
2017-01-30 17:17:17 +0100

    [migration] Fix crash on 3.24 migration
    Some sql using a column droped the line above crash migration of cw < 3.24.
    
    --HG--
    branch : 3.24

--- cubicweb/misc/migration/3.24.0_Any.py
+++ cubicweb/misc/migration/3.24.0_Any.py
@@ -6,14 +6,15 @@ add_entity_type('CWSession')
 rql('DELETE CWProperty X WHERE X pkey "system.version.pyramid"',
     ask_confirm=False)
 
-sql('DROP TABLE moved_entities')
-sql('ALTER TABLE entities DROP COLUMN asource')
 # before removing extid, ensure it's coherent with cwuri
 for eid, etype, encoded_extid in sql(
         "SELECT eid, type, extid FROM entities, cw_CWSource "
         "WHERE cw_CWSource.cw_name=entities.asource AND cw_CWSource.cw_type='ldapfeed'"):
     sql('UPDATE cw_{} SET cw_cwuri=%(cwuri)s WHERE cw_eid=%(eid)s'.format(etype),
         {'eid': eid, 'cwuri': b64decode(encoded_extid)})
+
+sql('DROP TABLE moved_entities')
+sql('ALTER TABLE entities DROP COLUMN asource')
 sql('ALTER TABLE entities DROP COLUMN extid')
 sql('DROP INDEX entities_type_idx')
 
