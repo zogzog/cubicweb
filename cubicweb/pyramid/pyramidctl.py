caf73424cb3a48c554c5db8b552f02a95559abc9
Denis Laxalde <denis.laxalde@logilab.fr>
2017-02-10 17:13:44 +0100

    [pyramid] Add a copyright and docstring to all modules
    We add copyright statements for both UNLISH (original author) and LOGILAB.

--- cubicweb/pyramid/pyramidctl.py
+++ cubicweb/pyramid/pyramidctl.py
@@ -1,9 +1,30 @@
+# copyright 2017 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2014-2016 UNLISH S.A.S. (Montpellier, FRANCE), all rights reserved.
+#
+# contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
+#
+# This file is part of CubicWeb.
+#
+# CubicWeb is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# CubicWeb is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with CubicWeb.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Provides a 'pyramid' command as a replacement to the 'start' command.
 
 The reloading strategy is heavily inspired by (and partially copied from)
 the pyramid script 'pserve'.
 """
+
 from __future__ import print_function
 
 import atexit
