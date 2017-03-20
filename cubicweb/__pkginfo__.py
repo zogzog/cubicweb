147af592b7f339b9403f2e123e63b503487f583e
Denis Laxalde <denis.laxalde@logilab.fr>
2017-03-20 17:04:10 +0100

    [pkg] Version 3.24.7
    --HG--
    branch : 3.24

--- cubicweb/__pkginfo__.py
+++ cubicweb/__pkginfo__.py
@@ -27,7 +27,7 @@ import glob
 
 modname = distname = "cubicweb"
 
-numversion = (3, 24, 6)
+numversion = (3, 24, 7)
 version = '.'.join(str(num) for num in numversion)
 
 description = "a repository of entities / relations for knowledge management"
