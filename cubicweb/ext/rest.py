943ca2c8a9a97c2d7178899f312068d7deae78d2
Denis Laxalde <denis.laxalde@logilab.fr>
2018-06-28 11:11:10 +0200

    [ext/rest] Drop "handle_io_errors" argument to docutils' FileInput
    This argument is deprecated and ignore in docutils since version 0.10.
    Besides, its default value is None which is the same as we passed.
    
    --HG--
    branch : 3.26

--- cubicweb/ext/rest.py
+++ cubicweb/ext/rest.py
@@ -229,7 +229,7 @@ def winclude_directive(name, arguments, options, content, lineno,
         include_file = io.FileInput(
             source_path=path, encoding=encoding,
             error_handler=state.document.settings.input_encoding_error_handler,
-            handle_io_errors=None)
+        )
     except IOError as error:
         severe = state_machine.reporter.severe(
               'Problems with "%s" directive path:\n%s: %s.'
