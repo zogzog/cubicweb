d419a632824376de0d7b7c8f7964ed1ea23eaf40
Philippe Pepiot <philippe.pepiot@logilab.fr>
2019-03-28 10:07:02 +0100

    [devtools/test] drop legacy cube i18n tests
    test_i18ncube_legacy_layout() is droped and make others tests uses a new style
    cube "cubicweb_i18ntestcube", so it doesn't depends on legacy cube machinery.

--- cubicweb/devtools/test/unittest_i18n.py
+++ cubicweb/devtools/test/unittest_i18n.py
@@ -58,7 +58,7 @@ def load_po(fname):
     return msgs
 
 
-TESTCUBE_DIR = osp.join(DATADIR, 'cubes', 'i18ntestcube')
+TESTCUBE_DIR = osp.join(DATADIR, 'libpython', 'cubicweb_i18ntestcube')
 
 
 class cubePotGeneratorTC(TestCase):
@@ -72,17 +72,6 @@ class cubePotGeneratorTC(TestCase):
             env['PYTHONPATH'] = ''
         env['PYTHONPATH'] += osp.join(DATADIR, 'libpython')
         cubedir = osp.join(DATADIR, 'libpython', 'cubicweb_i18ntestcube')
-        self._check(cubedir, env)
-
-    def test_i18ncube_legacy_layout(self):
-        env = os.environ.copy()
-        env['CW_CUBES_PATH'] = osp.join(DATADIR, 'cubes')
-        if 'PYTHONPATH' in env:
-            env['PYTHONPATH'] += os.pathsep
-        else:
-            env['PYTHONPATH'] = ''
-        env['PYTHONPATH'] += DATADIR
-        cubedir = osp.join(DATADIR, 'cubes', 'i18ntestcube')
         self._check(cubedir, env)
 
     def _check(self, cubedir, env):
@@ -137,19 +126,14 @@ class I18nCollectorTest(BaseTestCase):
     @patch('pkg_resources.load_entry_point', return_value=FakeMessageExtractor)
     def test_cube_custom_extractor(self, mock_load_entry_point):
         distname = 'cubicweb_i18ntestcube'  # same for new and legacy layout
-        for cubedir in [
-            osp.join(DATADIR, 'libpython', 'cubicweb_i18ntestcube'),
-            # Legacy cubes.
-            osp.join(DATADIR, 'cubes', 'i18ntestcube'),
-        ]:
-            with self.subTest(cubedir=cubedir):
-                with capture_stdout() as stream:
-                    devctl.update_cube_catalogs(cubedir)
-                self.assertIn(u'no message catalog for cube i18ntestcube',
-                              stream.read())
-                mock_load_entry_point.assert_called_once_with(
-                    distname, 'cubicweb.i18ncube', 'i18ntestcube')
-                mock_load_entry_point.reset_mock()
+        cubedir = osp.join(DATADIR, 'libpython', 'cubicweb_i18ntestcube')
+        with capture_stdout() as stream:
+            devctl.update_cube_catalogs(cubedir)
+        self.assertIn(u'no message catalog for cube i18ntestcube',
+                      stream.read())
+        mock_load_entry_point.assert_called_once_with(
+            distname, 'cubicweb.i18ncube', 'i18ntestcube')
+        mock_load_entry_point.reset_mock()
 
 
 if __name__ == '__main__':
