3460b24f0c0f90749e5cf55bd757a30e862fdd68
Laurent Peuch <cortex@worlddomination.be>
2019-05-15 12:05:52 +0200

    [pyramid/test] add test for pyramid.ini generation

--- cubicweb/pyramid/test/test_config.py
+++ cubicweb/pyramid/test/test_config.py
@@ -25,6 +25,7 @@ from unittest.mock import patch
 
 
 from cubicweb.pyramid import config
+from cubicweb.pyramid.pyramidctl import _generate_pyramid_ini_file
 
 
 class PyramidConfigTC(TestCase):
@@ -34,6 +35,27 @@ class PyramidConfigTC(TestCase):
             secret = config.get_random_secret_key()
         self.assertEqual(patched_choice.call_count, 50)
         self.assertEqual(secret, '0' * 50)
+
+    def test_write_pyramid_ini(self):
+        with TemporaryDirectory() as instancedir:
+            pyramid_ini_path = path.join(instancedir, "pyramid.ini")
+            with patch('random.SystemRandom.choice', return_value='0') as patched_choice:
+                _generate_pyramid_ini_file(pyramid_ini_path)
+            with open(path.join(instancedir, 'pyramid.ini')) as f:
+                lines = f.readlines()
+
+        self.assertEqual(patched_choice.call_count, 50 * 3)
+
+        secret = '0' * 50
+
+        for option in ('cubicweb.session.secret',
+                       'cubicweb.auth.authtkt.persistent.secret',
+                       'cubicweb.auth.authtkt.session.secret'):
+            self.assertIn('{} = {}\n'.format(option, secret), lines)
+
+        for option in ('cubicweb.auth.authtkt.persistent.secure',
+                       'cubicweb.auth.authtkt.session.secure'):
+            self.assertIn('{} = {}\n'.format(option, "no"), lines)
 
     def test_write_development_ini(self):
         with TemporaryDirectory() as instancedir:
