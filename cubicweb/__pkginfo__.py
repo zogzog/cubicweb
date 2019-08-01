ed1ee40b9fdfeeb61b84d5623748013de224f86e
Denis Laxalde <denis.laxalde@logilab.fr>
2019-08-01 09:13:52 +0200

    [pkg] Version 3.27.0.a2

--- cubicweb/__pkginfo__.py
+++ cubicweb/__pkginfo__.py
@@ -23,7 +23,7 @@ software
 modname = distname = "cubicweb"
 
 numversion = (3, 27, 0)
-version = '.'.join(str(num) for num in numversion) + 'a1'
+version = '.'.join(str(num) for num in numversion) + 'a2'
 
 description = "a repository of entities / relations for knowledge management"
 author = "Logilab"
