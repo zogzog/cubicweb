86882bae23e01b2011bd1b4298c5a261db36b32a
Philippe Pepiot <philippe.pepiot@logilab.fr>
2019-12-10 17:14:56 +0100

    [pkg] Version 3.27.0rc2

--- cubicweb/__pkginfo__.py
+++ cubicweb/__pkginfo__.py
@@ -23,7 +23,7 @@ software
 modname = distname = "cubicweb"
 
 numversion = (3, 27, 0)
-version = '.'.join(str(num) for num in numversion) + 'rc1'
+version = '.'.join(str(num) for num in numversion) + 'rc2'
 
 description = "a repository of entities / relations for knowledge management"
 author = "Logilab"
